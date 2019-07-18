222058f26d1c2bfd9576c203f6a07e3de468bd5a
Ivan Bodnya <ivanbodnya@gmail.com>
2019-07-18 19:09:12 +0300

    Compeleted draft structure of the BOKWEb project.

--- BOKDatabase/dbo/Tables/Posting.sql
+++ BOKDatabase/dbo/Tables/Posting.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Posting]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY, 
     [Title] NVARCHAR(50) NOT NULL, 
     [Author] NVARCHAR(50) NOT NULL, 
-    [DateCreated] DATETIME2 NOT NULL, 
-    [Description] NVARCHAR(50) NOT NULL, 
+    [DateCreated] DATETIME2 NOT NULL DEFAULT getdate(), 
+    [Description] NVARCHAR(MAX) NOT NULL, 
     [MinToRead] NVARCHAR(50) NOT NULL 
 )
